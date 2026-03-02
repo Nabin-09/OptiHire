38f8d54e7494abd9e139092640723dd6ce4900cb
Nabin <sharmanabin.0910@gmail.com>
2026-03-03 01:20:13 +0530

    Chore : axios calls for get-me and logout

--- frontend/src/features/auth/auth.api.js
+++ frontend/src/features/auth/auth.api.js
@@ -23,8 +23,30 @@ export async function login({email, password}){
             withCredentials : true
         });
 
-        return response;
+        return response.data;
      }catch(err){
         console.log(`Axios error : ${err}`)
      }
+}
+
+export async function logout(){
+    try{
+        const response = await axios.get('http://localhost:5000/api/auth/logout' , {
+            withCredentials : true
+        })
+        return response.data
+    }catch(err){
+        console.log(`${err}`)
+    }
+}
+
+export async function getMe(){
+    try{
+        const response = await axios.get('http://localhost:5000/api/auth/get-me' , {
+            withCredentials : true
+        });
+        return response.data;
+    }catch(err){
+        console.log(`Axios error : ${err}`)
+    }
 }
