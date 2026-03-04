372f3989c177e9d45012c87bd8d6385d152630a3
Nabin <sharmanabin.0910@gmail.com>
2026-03-04 21:50:48 +0530

    chore : pdf handling via multer

--- backend/src/routes/interview.routes.js
+++ backend/src/routes/interview.routes.js
@@ -1,6 +1,7 @@
 import express from "express";
 import { authUser } from "../middlewares/auth.middleware";
 export const interviewRouter = express.Router();
+import { interviewController } from "../controllers/interview.controller";
 
 
 /**
@@ -8,4 +9,4 @@ export const interviewRouter = express.Router();
  * @description generate new interview report on basis of user self description , resume and Job Description
  * @access PRIVATE
  */
-interviewRouter.post('/' , authUser , )
+interviewRouter.post('/' , authUser , interviewController)
